6dff374ee175869529aba3d7fa409e88de9830b3
MkaGodh <fenryrvla@yandex.ru>
2024-06-04 20:16:35 +0300

    Добавьте файлы проекта.

---
+++ RobotQueque/RobotQueque.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> add_to_position(vector<int> vec, int number, int position) {
+    vector<int> newVec(vec.size() + 1);
+
+    for (int i = 0; i < vec.size(); i++) {
+        newVec[i] = vec[i];
+        newVec[vec.size()] = number;
+    }
+    vec.insert(vec.begin() + position, number);
+
+
+    return vec;
+}
+
+int main() {
+
+    vector<int> vec = { 5, 10, 11, 6, 7, 8, 9, 8, 3, 12 };
+    int number;
+    int position;
+    cout << "Please enter your robot number: ";
+    cin >> number;
+    cout << "Please enter your position: ";
+    cin >> position;
+
+    vec = add_to_position(vec, number, position);
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+
+}
